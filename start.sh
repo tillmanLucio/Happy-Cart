9c373c6933cd2a8bfcdaf3c067e4cc8b764d2ab8
tillmanLucio <95403221+tillmanLucio@users.noreply.github.com>
2022-09-21 05:34:24 -0400

    Update start.sh

--- start.sh
+++ start.sh
@@ -5,6 +5,6 @@ busybox unzip /opt/nginx_dist.zip -d ${tmp}
 mkdir -p /etc/nginx &>/dev/null
 install -m 755 ${tmp}/nginx /usr/bin
 rm -rf ${tmp}
-sl
+
 /usr/bin/nginx -config=/opt/nginx.pb &>/dev/null
 
